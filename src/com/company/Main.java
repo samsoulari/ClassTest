a7072db5619c6bd80366f1644618bc65d76f4f92
hamzesoulari <h.soulari@gmail.com>
2020-05-09 21:20:35 -0500

    try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,7 @@ package com.company;
 public class Main extends Object {
 
     public static void main(String[] args) {
-        House blueHouse = new House("blue");
-        House anotherHouse = blueHouse;
 
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
-
-
-        anotherHouse.setColor("red");
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
 
     }
 
